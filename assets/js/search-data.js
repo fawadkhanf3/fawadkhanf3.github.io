ec2f20b565801d517155c11a1384be838db40a16
fawadkhanf3 <61941495+fawadkhanf3@users.noreply.github.com>
2026-01-30 15:24:14 +0000

    Deploying to gh-pages from @ fawadkhanf3/fawadkhanf3.github.io@3ff5230cb11a54096c6b788f82275214e1bf9999 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,13 +23,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-cv",
-          title: "CV",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/cv/";
-          },
         },{id: "nav-teaching",
           title: "teaching",
           description: "Course materials, schedules, and resources for classes taught.",
